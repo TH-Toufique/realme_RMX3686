8ce2ed87879c4bfb04b1b0000f426bbeecc8f91c
bengris32 <bengris32@protonmail.ch>
2024-02-13 09:08:27 +0530

    Build default renderscript impl

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.6.vendor
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Rootdir
 PRODUCT_PACKAGES += \
     factory_init.connectivity.common.rc \
