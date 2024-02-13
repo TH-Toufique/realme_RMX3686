76a24dc103d940bb748626732b123d51c06d95f4
luk1337 <priv.luk@gmail.com>
2024-02-13 19:29:11 +0530

    Import sensors/2.0/multihal

--- device.mk
+++ device.mk
@@ -309,10 +309,9 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
-    android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.1.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-service-multihal.ivan \
     libsensorndkbridge
 
 # Secure Element
